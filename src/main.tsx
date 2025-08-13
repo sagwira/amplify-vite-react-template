05506409a5c873ec7d84954a70ac53e54effd2f2
sagwira <shaun.gwira@gmail.com>
2025-08-13 01:22:06 +0100

    added per-user data isolation

--- src/main.tsx
+++ src/main.tsx
@@ -9,13 +9,10 @@ import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(  
+ReactDOM.createRoot(document.getElementById("root")!).render(   
   <React.StrictMode>
     <Authenticator>
       <App />
     </Authenticator>
-  </React.StrictMode>
-  <React.StrictMode>
-    <App />
   </React.StrictMode>
 );
